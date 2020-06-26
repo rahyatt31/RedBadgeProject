f4679fcbfb4cdbf2f9c35df182ce64d12e77d457
rahyatt <rahyatt31@gmail.com>
2020-06-26 14:27:14 -0400

    Can create a game now!

---
+++ GameLibrary.Data/Migrations/202006261823468_June26.cs
@@ -0,0 +1,24 @@
+﻿namespace GameLibrary.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class June26 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Game", "GameMultiplayer", c => c.Boolean(nullable: false));
+            AddColumn("dbo.Game", "GameOnline", c => c.Boolean(nullable: false));
+            DropColumn("dbo.Game", "Multiplayer");
+            DropColumn("dbo.Game", "Online");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Game", "Online", c => c.Boolean(nullable: false));
+            AddColumn("dbo.Game", "Multiplayer", c => c.Boolean(nullable: false));
+            DropColumn("dbo.Game", "GameOnline");
+            DropColumn("dbo.Game", "GameMultiplayer");
+        }
+    }
+}
